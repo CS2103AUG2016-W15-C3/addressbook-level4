f942053b14c3ced7d8ff9db130bca4e4e26c4dad
“marcusngwj” <marcusngwj@msn.com>
2016-10-10 11:33:02 +0800

    Update AddCommand to support TaskDate

--- src/main/java/seedu/taskell/logic/commands/AddCommand.java
+++ src/main/java/seedu/taskell/logic/commands/AddCommand.java
@@ -16,9 +16,9 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a task to the task manager. "
-            + "Parameters: Description [t/TAG]...\n"
+            + "Parameters: Description by DATE [t/TAG]...\n"
             + "Example: " + COMMAND_WORD
-            + " Jogging with Peter t/friends t/owesMoney";
+            + " Jogging with Peter by 11-10-2106 t/friends t/owesMoney";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the task manager";
@@ -30,14 +30,16 @@ public class AddCommand extends Command {
      *
      * @throws IllegalValueException if any of the raw values are invalid
      */
-    public AddCommand(String description, Set<String> tags)
+    public AddCommand(String description, TaskDate taskDate, Set<String> tags)
             throws IllegalValueException {
         final Set<Tag> tagSet = new HashSet<>();
         for (String tagName : tags) {
             tagSet.add(new Tag(tagName));
         }
+        System.out.println("DES: " + description);
         this.toAdd = new Task(
                 new Description(description),
+                taskDate,
                 new UniqueTagList(tagSet)
         );
     }
