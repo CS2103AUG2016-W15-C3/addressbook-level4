10abf12d23b6c1c79fdfaa09470e306448138722
“marcusngwj” <marcusngwj@msn.com>
2016-10-11 01:31:14 +0800

    Update from add_TaskStub

---
+++ src/main/java/seedu/taskell/stubs/TaskDateStub.java
@@ -0,0 +1,8 @@
+package seedu.taskell.stubs;
+
+/** Stub class for TaskDate
+ *  TODO: delete when TaskDate is ready
+ * */
+public class TaskDateStub {
+
+}
---
+++ src/main/java/seedu/taskell/stubs/TaskTimeStub.java
@@ -0,0 +1,9 @@
+package seedu.taskell.stubs;
+
+/** Stub class for TaskTime
+ *  TODO: delete when TaskTime is ready
+ */
+
+public class TaskTimeStub {
+
+}
